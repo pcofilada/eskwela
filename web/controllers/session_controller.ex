9f1a6d7e125c2f54339b70e25e8642d61234a8ef
Patrick Ofilada <pcofilada@gmail.com>
2016-08-20 02:49:49 +0800

    Add logout button

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -2,7 +2,7 @@ defmodule Eskwela.SessionController do
   use Eskwela.Web, :controller
 
   plug :put_layout, "authentication.html"
-  plug :check_if_logged_in
+  plug :check_if_logged_in when not action in [:delete]
 
   def new(conn, __params) do
     render(conn, "new.html")
@@ -11,10 +11,7 @@ defmodule Eskwela.SessionController do
   def create(conn, %{"session" => session_params}) do
     case Eskwela.Auth.sign_in(session_params, Repo) do
       {:ok, user} ->
-        conn
-        |> put_session(:user_id, user.id)
-        |> put_flash(:info, "Signed In")
-        |> redirect(to: quiz_path(conn, :index))
+        check_role(conn, user)
       :error ->
         conn
         |> put_flash(:error, "Wrong email or password")
@@ -27,5 +24,20 @@ defmodule Eskwela.SessionController do
     |> delete_session(:user_id)
     |> put_flash(:info, "Signed Out")
     |> redirect(to: "/")
+  end
+
+  defp check_role(conn, user) do
+    case user.role do
+      "users" ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> put_flash(:info, "Signed In")
+        |> redirect(to: quiz_path(conn, :index))
+      "admin" ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> put_flash(:info, "Signed In")
+        |> redirect(to: level_path(conn, :index))
+    end
   end
 end
